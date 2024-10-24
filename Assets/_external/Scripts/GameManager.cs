8818c9268d0fe58e93b51288076f5e8a74b209bf
Jessica Arrivabem <jessica.arrivabem@gmail.com>
2024-10-24 20:13:42 -0300

    Added Collectables

--- Assets/_external/Scripts/GameManager.cs
+++ Assets/_external/Scripts/GameManager.cs
@@ -4,11 +4,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public InputManager InputManager { get; private set; }
+
+    [Header("Dynamic Game Objects")]
+    [SerializeField] private GameObject bossDoor;
+
     [Header("Managers")]
     public UIManager UIManager;
-
     public AudioManager AudioManager;
-    public InputManager InputManager { get; private set; }
+
+    private int totalKeys;
+    private int keysLeftToCollect;
+
 
     // Start is called before the first frame update
     private void Awake()
@@ -16,7 +23,24 @@ public class GameManager : MonoBehaviour
         if (Instance != null) Destroy(this.gameObject);
         Instance = this;
 
+        totalKeys = FindObjectsOfType<CollectableKey>().Length;
+        keysLeftToCollect = totalKeys;
+
         InputManager = new InputManager();
+    }
+
+    public void UpdateKeysLeft()
+    {
+        keysLeftToCollect--;
+        CheckAllKeysCollected();
+    }
+
+    private void CheckAllKeysCollected()
+    {
+        if(keysLeftToCollect <= 0)
+        {
+            Destroy(bossDoor);
+        }
     }
 
 }
